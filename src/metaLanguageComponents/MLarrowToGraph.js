79673cd7a0aed0cfb3794932930122302e8b38bc
valterUo <valter.uotila@gmail.com>
2019-03-28 16:41:36 +0200

    multiple edges between ML arrow graph

--- src/metaLanguageComponents/MLarrowToGraph.js
+++ src/metaLanguageComponents/MLarrowToGraph.js
@@ -8,7 +8,7 @@ import data4 from '../exampleData/data4'
 class MLarrowToGraph extends Component {
     constructor(props) {
         super(props)
-        this.state = {source: "", target: "", data: {nodes: [], links: []}, show: false}
+        this.state = { source: "", target: "", data: { nodes: [], links: [] }, show: false }
     }
 
     handleSourceChange = (event) => {
@@ -23,21 +23,36 @@ class MLarrowToGraph extends Component {
         })
     }
 
+    countLinks(links) {
+        links.map(linkObject => {
+            links.map(linkObject2 => {
+                if (JSON.stringify(linkObject) !== JSON.stringify(linkObject2)) {
+                    if ((linkObject.source === linkObject2.source && linkObject.target === linkObject2.target) || (linkObject.target === linkObject2.source && linkObject.source === linkObject2.target)) {
+                        linkObject2.count = linkObject2.count + 1
+                    }
+                }
+                return linkObject2
+            })
+            return linkObject
+        })
+        return links
+    }
+
     initializeCode(text) {
         return text.split(";")
-        .join("")
-        .split("\n")
-        .join("")
-        .split("|")
-        .map(element => element.trim())
-        .map(pair => pair.split("=>").map(element => element.trim()))
+            .join("")
+            .split("\n")
+            .join("")
+            .split("|")
+            .map(element => element.trim())
+            .map(pair => pair.split("=>").map(element => element.trim()))
     }
 
     constructLinks(nodes, sourceLinks, targetLinks) {
         let links = sourceLinks.map(link => {
             let linkPairIndex = { name: link[0], source: "", target: "", count: 1 }
             nodes.map((node, i) => {
-                if(node.name === link[1]) {
+                if (node.name === link[1]) {
                     linkPairIndex.source = i
                 }
                 return node
@@ -49,9 +64,9 @@ class MLarrowToGraph extends Component {
 
         links.map(linkObject => {
             targetLinks.map(targetLink => {
-                if(targetLink[0] === linkObject.name) {
+                if (targetLink[0] === linkObject.name) {
                     nodes.map(node => {
-                        if(node.name === targetLink[1]) {
+                        if (node.name === targetLink[1]) {
                             linkObject.target = node.id
                         }
                         return node
@@ -71,30 +86,32 @@ class MLarrowToGraph extends Component {
         let nodesSet = new Set([])
         sourceLinks.map(pair => nodesSet.add(pair[1]))
         targetLinks.map(pair => nodesSet.add(pair[1]))
-        let nodes = [...nodesSet].map((element, i) => {return {name: element, id: i}})
+        let nodes = [...nodesSet].map((element, i) => { return { name: element, id: i } })
         let links = this.constructLinks(nodes, sourceLinks, targetLinks)
-        return {nodes: nodes, links: links}
+        links = this.countLinks(links)
+        console.log(links)
+        return { nodes: nodes, links: links }
     }
 
     handleCodeSubmit = (event) => {
         event.preventDefault()
-        if(this.state.show === true) {
-            this.setState({ show: false})
+        if (this.state.show === true) {
+            this.setState({ show: false })
         }
         let data = this.constructNodesAndLinks()
-        
+
         this.setState({
             source: "",
             target: ""
         })
         this.setState({
-            data: {"nodes": data.nodes, "links": data.links}
-        }, () => {this.setState({show: true})})  
+            data: { "nodes": data.nodes, "links": data.links }
+        }, () => { this.setState({ show: true }) })
     }
 
     render() {
         return <div>
-            <Form onSubmit = {this.handleCodeSubmit}>
+            <Form onSubmit={this.handleCodeSubmit}>
                 <Form.Group controlId="MLsource">
                     <Form.Label>Enter ML definition of source function</Form.Label>
                     <Form.Control as="textarea" rows="5" value={this.state.source} onChange={this.handleSourceChange} />
@@ -103,11 +120,11 @@ class MLarrowToGraph extends Component {
                     <Form.Label>Enter ML definition of target function</Form.Label>
                     <Form.Control as="textarea" rows="5" value={this.state.target} onChange={this.handleTargetChange} />
                 </Form.Group>
-                <Button type = "submit" variant = "dark">Construct graph</Button>
+                <Button type="submit" variant="dark">Construct graph</Button>
             </Form>
-        {(this.state.show === true) &&
-        <MultiGraph key={"MLcategory"} id="100" data={this.state.data} width={1500} height={1500} nodeName={"MLnodes"} linkName={"MLlinks"} nameClass={"MLGraph"} />}
-            </div>
+            {(this.state.show === true) &&
+                <MultiGraph key={"MLcategory"} id="100" data={this.state.data} width={1500} height={1500} nodeName={"MLnodes"} linkName={"MLlinks"} nameClass={"MLGraph"} />}
+        </div>
     }
 }
 
