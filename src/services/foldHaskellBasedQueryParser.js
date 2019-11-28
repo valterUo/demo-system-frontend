79541239e46804ac924f4578993cc75b90fef4ab
valterUo <valter.uotila@gmail.com>
2019-11-28 17:54:22 +0200

    Remove AS clause from query definition

--- src/services/foldHaskellBasedQueryParser.js
+++ src/services/foldHaskellBasedQueryParser.js
@@ -1,5 +1,6 @@
 /* eslint-disable array-callback-return */
 import haskellCompiler from '../services/haskellCompilerService'
+import sourceModels from "./collectionDataTypesForDatasets.json"
 
 const parseLetInQueryBlock = (inputString) => {
     if (inputString.includes("LET")) {
@@ -29,9 +30,9 @@ const parseLetInQueryBlock = (inputString) => {
             }
             return query
         })
-        if(mainQuery !== undefined && mainQuery !== "Error!") {
+        if (mainQuery !== undefined && mainQuery !== "Error!") {
             mainQuery["query"] = parsedSubQueries + mainQuery["query"]
-        return mainQuery
+            return mainQuery
         }
     } else {
         return parseMainQueryBlock(inputString)
@@ -55,7 +56,7 @@ const parseMainQueryBlock = (inputString) => {
         .split("@")
     queryElements.shift()
     queryElements = queryElements.map(element => element.trim())
-    if (queryElements.length !== 5) {
+    if (queryElements.length !== 4) {
         return {
             "model": "error",
             "message": "Error! Wrong amount of arguments in the query."
@@ -68,23 +69,37 @@ const parseMainQueryBlock = (inputString) => {
         }
     }
     console.log(queryElements)
-    switch (queryElements[2]) {
+    const lambdafunction = queryElements[0]
+    const dataset = queryElements[1]
+    const sourceModel = sourceModels[dataset]
+    console.log(sourceModel)
+    switch (sourceModel) {
         case "relational":
         case "xml":
         case "json":
             haskellQuery.push("foldr ")
-            switch (queryElements[3]) {
+            switch (queryElements[2]) {
                 case "relational":
                 case "xml":
                 case "json":
-                    haskellQuery.push(queryElements[0])
+                    haskellQuery.push(lambdafunction)
                     haskellQuery.push(" [] ")
-                    haskellQuery.push(queryElements[1])
+                    haskellQuery.push(dataset)
                     break
                 case "graph":
-                    haskellQuery.push(queryElements[0])
+                    haskellQuery.push(lambdafunction)
                     haskellQuery.push(" Algebra.Graph.empty ")
-                    haskellQuery.push(queryElements[1])
+                    haskellQuery.push(dataset)
+                    break
+                case "rdf":
+                    haskellQuery.push(lambdafunction)
+                    haskellQuery.push(" RDF.empty ")
+                    haskellQuery.push(dataset + " :: RDF TList)")
+                    break
+                case "nimblegraph":
+                    haskellQuery.push(lambdafunction)
+                    haskellQuery.push(" emptyNimbleGraph ")
+                    haskellQuery.push(dataset)
                     break
                 default:
                     return "Error!"
@@ -92,22 +107,36 @@ const parseMainQueryBlock = (inputString) => {
             break
         case "graph":
             haskellQuery.push("foldg ")
-            switch (queryElements[3]) {
+            switch (queryElements[2]) {
                 case "relational":
                 case "xml":
                 case "json":
                     haskellQuery.push(" [] ")
-                    haskellQuery.push(queryElements[0])
+                    haskellQuery.push(lambdafunction)
                     haskellQuery.push(" (union) ")
                     haskellQuery.push("(union) ")
-                    haskellQuery.push(queryElements[1])
+                    haskellQuery.push(dataset)
                     break
                 case "graph":
                     haskellQuery.push("Algebra.Graph.empty ")
-                    haskellQuery.push(queryElements[0])
+                    haskellQuery.push(lambdafunction)
                     haskellQuery.push(" overlay ")
                     haskellQuery.push("connect ")
-                    haskellQuery.push(queryElements[1])
+                    haskellQuery.push(dataset)
+                    break
+                case "rdf":
+                    haskellQuery.push(" RDF.empty ")
+                    haskellQuery.push(lambdafunction)
+                    haskellQuery.push(" rdfUnion ")
+                    haskellQuery.push("rdfUnion ")
+                    haskellQuery.push(dataset + " :: RDF TList)")
+                    break
+                case "nimblegraph":
+                    haskellQuery.push(" emptyNimbleGraph ")
+                    haskellQuery.push(lambdafunction)
+                    haskellQuery.push(" nimbleGraphUnion ")
+                    haskellQuery.push("nimbleGraphUnion ")
+                    haskellQuery.push(dataset)
                     break
                 default:
                     return "Error!"
@@ -115,30 +144,63 @@ const parseMainQueryBlock = (inputString) => {
             break
         case "rdf":
             haskellQuery.push("(foldrdf ")
-            switch (queryElements[3]) {
+            switch (queryElements[2]) {
                 case "relational":
                 case "xml":
                 case "json":
                     haskellQuery.push(" [] ")
-                    haskellQuery.push(queryElements[0])
+                    haskellQuery.push(lambdafunction)
                     haskellQuery.push(" (union) ")
                     haskellQuery.push("(union) ")
-                    haskellQuery.push(queryElements[1])
+                    haskellQuery.push(dataset)
                     break
                 case "graph":
                     haskellQuery.push("Algebra.Graph.empty ")
-                    haskellQuery.push(queryElements[0])
+                    haskellQuery.push(lambdafunction)
                     haskellQuery.push(" overlay ")
                     haskellQuery.push("connect ")
-                    haskellQuery.push(queryElements[1])
+                    haskellQuery.push(dataset)
                     break
                 case "rdf":
-                    haskellQuery.push(queryElements[0])
+                    haskellQuery.push(lambdafunction)
                     haskellQuery.push(" RDF.empty ")
-                    haskellQuery.push(queryElements[1] + " :: RDF TList)")
+                    haskellQuery.push(dataset + " :: RDF TList)")
                     break
                 default:
                     return "Error!"
+            }
+            break
+        case "nimblegraph":
+            haskellQuery.push("(foldNimble ")
+            switch (queryElements[2]) {
+                case "relational":
+                case "xml":
+                case "json":
+                    haskellQuery.push(" [] ")
+                    haskellQuery.push(lambdafunction)
+                    haskellQuery.push(" (union) ")
+                    haskellQuery.push("(union) ")
+                    haskellQuery.push(dataset)
+                    break
+                case "graph":
+                    haskellQuery.push("Algebra.Graph.empty ")
+                    haskellQuery.push(lambdafunction)
+                    haskellQuery.push(" overlay ")
+                    haskellQuery.push("connect ")
+                    haskellQuery.push(lambdafunction)
+                    break
+                case "rdf":
+                    haskellQuery.push(lambdafunction)
+                    haskellQuery.push(" RDF.empty ")
+                    haskellQuery.push(dataset + " :: RDF TList)")
+                    break
+                case "nimblegraph":
+                    haskellQuery.push(lambdafunction)
+                    haskellQuery.push(" emptyNimbleGraph ")
+                    haskellQuery.push(dataset)
+                    break
+                default:
+                    return "Error! Target model did not match any model."
             }
             break
         default:
@@ -148,62 +210,62 @@ const parseMainQueryBlock = (inputString) => {
     }
 
     return {
-        "model": queryElements[3],
+        "model": queryElements[2],
         "query": haskellQuery.join("")
     }
 }
 
 const executeQuery = async (inputQuery) => {
     const parsedQueryList = parseLetInQueryBlock(inputQuery)
-    if(parsedQueryList === undefined) {
+    if (parsedQueryList === undefined) {
         return {
             "model": "error",
             "message": "The query is not valid."
         }
     } else {
-    const model = parsedQueryList["model"]
-    const query = parsedQueryList["query"]
-    console.log(query)
-    if (model === "graph") {
-        return {
-            "model": model,
-            "answer": await haskellCompiler.compileGraphQuery(query),
-            "fold": query
-        }
-    } else if (model === "relational") {
-        return {
-            "model": model,
-            "answer": await haskellCompiler.compileRelationalQuery(query),
-            "fold": query
-        }
-    } else if (model === "xml") {
-        return {
-            "model": model,
-            "answer": await haskellCompiler.compileTreeQuery(query),
-            "fold": query
-        }
-    } else if(model === "rdf") {
-        return {
-            "model": "graph",
-            "answer": await haskellCompiler.compileRDFGraphQuery(query),
-            "fold": query
-        }
-    } else if(model === "json") {
-        return {
-            "model": "relational",
-            "answer": await haskellCompiler.compileRelationalQuery(query),
-            "fold": query
-        }
-    } else if (model === "error") {
-        return parsedQueryList
-    } else {
-        return {
-            "model": "error",
-            "message": "Unexpected error happened. No model was found.",
-            "fold": query
+        const model = parsedQueryList["model"]
+        const query = parsedQueryList["query"]
+        console.log(query)
+        if (model === "graph") {
+            return {
+                "model": model,
+                "answer": await haskellCompiler.compileGraphQuery(query),
+                "fold": query
+            }
+        } else if (model === "relational") {
+            return {
+                "model": model,
+                "answer": await haskellCompiler.compileRelationalQuery(query),
+                "fold": query
+            }
+        } else if (model === "xml") {
+            return {
+                "model": model,
+                "answer": await haskellCompiler.compileTreeQuery(query),
+                "fold": query
+            }
+        } else if (model === "rdf") {
+            return {
+                "model": "graph",
+                "answer": await haskellCompiler.compileRDFGraphQuery(query),
+                "fold": query
+            }
+        } else if (model === "json") {
+            return {
+                "model": "relational",
+                "answer": await haskellCompiler.compileRelationalQuery(query),
+                "fold": query
+            }
+        } else if (model === "error") {
+            return parsedQueryList
+        } else {
+            return {
+                "model": "error",
+                "message": "Unexpected error happened. No model was found.",
+                "fold": query
+            }
         }
     }
-}
 }
 
 export default {
